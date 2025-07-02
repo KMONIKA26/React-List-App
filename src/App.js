2bd050d8230c5705067cf07d171ca3a68173f530
Your Name <you@example.com>
2025-07-02 20:57:33 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Favorite Topics</h1>
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+// This is a simple React application that displays a list of favorite topics.
+// The App component renders a header and an unordered list with three items.
+// The CSS file is imported to style the application, and a logo is included but not used in this example.
